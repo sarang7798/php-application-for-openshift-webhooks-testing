971005a28a5575c1dd7b358a08ee409d6426e9b8
Sarang <birhadesarang@gmail.com>
2020-12-02 23:55:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <body>
 
 <h1>My first PHP page for UNNATI WEBHOOK S.B. test v 2.1.2</h1>
-  <marquee> Learning WEBHOOKS</marquee>
+  
 <?php
 echo "Hello World! from GIT";
 echo "Hello from version1.1  ";
-echo " Why to Worry ? S2I in openshift";
+  echo " Why to Worry ? S2I in openshift";
 ?>
 
 </body>
